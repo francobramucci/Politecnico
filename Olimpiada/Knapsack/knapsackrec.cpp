c40116c4b0ce3d4d7531f3a417aa4d62665f5593
francobramucci <bramuccifranco1@gmail.com>
2022-11-18 21:17:15 -0300

    knapsack

--- Olimpiada/Knapsack/knapsackrec.cpp
+++ Olimpiada/Knapsack/knapsackrec.cpp
@@ -1,18 +1,26 @@
 #include <bits/stdc++.h>
- 
+
 using namespace std;
- 
-#define forr(i,a,b) for(int i = int(a); i < int(b); i++)
+
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define forr(i,a,b) for(int i = (a); i < int(b); ++i)
 #define forn(i,n) forr(i,0,n)
+#define sz(v) int(v.size())
 #define endl '\n'
+#define fi first
+#define se second
 #define pb push_back
- 
+using ll = long long;
+using pii = pair<int,int>;
+using vi = vector<int>;
+
 const int MOD = 1e9+7;
-const int MAXD = 100000+100, MAXL = 1000+100;
-vector<int> precios;
-vector<int> paginas;
-int dp[MAXD][MAXL];
+const int MAXD = 100000+100;
+
+int dp[MAXD];
  
+// Recursive:
+/*
 int knapsack(int dinero, int libro){
 	if(dinero == 0 || libro == 0) return 0;
 	if(dp[dinero][libro] != -1) return dp[dinero][libro];
@@ -21,11 +29,14 @@ int knapsack(int dinero, int libro){
 	else 
 		return dp[dinero][libro] = knapsack(dinero, libro-1) % MOD;
 }
- 
+*/
+
 int main(){
+	fastio;
 	int n,x; cin >> n >> x;
-	memset(dp, -1, sizeof(dp));
-	
+	vector<int> precios;
+	vector<int> paginas;
+
 	precios.pb(0);
 	forn(i,n){
 		int v; cin >> v;
@@ -36,6 +47,16 @@ int main(){
 		int v; cin >> v;
 		paginas.pb(v);
 	}
- 	
- 	for()
+ // Iterative:
+
+ 	forr(i,1,n+1){ // 1: precio = 4, paginas = 5
+ 		for(int j = x; j>0; j--){
+ 			if(j >= precios[i]){
+ 				dp[j] = max(dp[j-precios[i]] + paginas[i], dp[j]);
+ 			}
+ 		}
+ 	}
+ 	cout << dp[x] << endl; 
+
+ 	return 0;
 }
