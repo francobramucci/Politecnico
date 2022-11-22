1b4e40dc71f85b5ae7bb54a7b42fa1552cba7cc7
francobramucci <bramuccifranco1@gmail.com>
2022-11-22 15:39:25 -0300

    problemas

---
+++ Olimpiada/Array Drescription/arraydesc.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define forr(i,a,b) for(int i = (a); i < int(b); ++i)
+#define forn(i,n) forr(i,0,n)
+#define sz(v) int(v.size())
+#define endl '\n'
+#define fi first
+#define se second
+#define pb push_back
+using ll = long long;
+using pii = pair<int,int>;
+using vi = vector<int>;
+
+const int MOD = 1e9+7;
+
+int solve(int begin, int ){
+
+}
+
+
+int main(){
+	int n,m; cin >> n >> m;
+	vector<int> num;
+
+	forn(i,n){
+		int v; cin >> v; 
+		num.pb(v);
+	}
+
+}
