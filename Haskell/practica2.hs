596fa010ec4d1d42622dc9ab54d22ad4df3f724e
francobramucci <bramuccifranco1@gmail.com>
2023-05-17 23:00:31 -0300

    Notas sobre lo hablado en clase

--- Haskell/practica2.hs
+++ Haskell/practica2.hs
@@ -110,3 +110,114 @@ completo e n = N (completo e (n-1)) e (completo e (n-1))
 
 completo e n = let t = completo e (n-1) in (N t e t)
 
+data CList a = EmptyCL | CUnit a | Consnoc a (CList) a
+
+[] = EmptyCL
+[1] = CUnit 2
+[1,2] = Conscnoc 1 EmptyCL 2
+[1,2,3] = Consnoc 1 (CUnit 2) 3
+[a,b,c,d] = Consnoc 'a' (Consnoc b EmptyCL c) d
+
+headCL (CUnit x) = xs
+headCL (Consoc x xs y) = x
+
+tailCL (CUnit x) = x
+tailCL (Consnoc x EmptyCL y) = CUnit y
+tailCL (Consnoc x xs y) = Consnoc (headCL xs) (tailCL) xs y
+
+reverseCL EmptyCL = EmptyCL
+reverseCL (CUnit x) = CUnit x
+reverseCL (Consnoc x xs y) = Consnoc y (reverseCL xs) x
+
+foldr f e [] = e
+foldr f e (x:xs) = x 'f' (foldr e xs)
+
+sum = foldr (+) 0
+-- Cuando no pongo el argumento se llama transparencia referencial
+
+length'= foldr (\x xs = 1+xs) 0
+
+-- Invariantes RBtree, Leftist Heaps, Heaps
+
+-- Traza
+foldr (\x xs = 1 + xs) 0 ['a':['b']] 
+                                    <por def 2.fodlr>
+(\x xs = 1 + xs) 'a' (foldr (\x xs = 1 + xs) 0 ['b'])
+1 + foldr (\x xs = 1 + xs) 0 ('b':[])
+                                    <por def 2.fodlr>
+1 + (\x xs = 1 + xs) 'b' (foldr (\x xs = 1 + xs) 0 [])
+1+(1+foldr fcualquiera 0 [])
+                                    <por def 1.foldr>
+1+(1+0) = 2
+
+--Tecnica tupling
+
+balanceado 0 x= E
+balanceado n x | even (n-1) = 
+                            let m = div (n-1) 2
+                                t = balanceado m x
+                            in N t x t
+               | otherwise = let m = div (n-1) 2
+                             in N balanceado (m+1) x balanceado m x
+
+--  otherwise lo cambio por
+
+= let m = div (n-1) 2
+    (t1,t2) = balanceado' m x
+    in N t1 x t2
+
+where balanceado' m x = (balanceado)
+
+-- Convertir en BST 
+-- let xs = [3,5,8,11,15,17,20,21]
+--            0,1,2,3,4,5,6,7,8
+-- length xs = 8;
+-- -- Agarramos el elemento del medio (15) para balancear el arbol
+-- 15 = xs !! div 8 2
+
+-- zs = [3,5,8,11] = take 4
+-- ys = [17,20,21] = drop (4+1) xs
+
+-- N zs 15 ys
+
+--Algoritmo
+
+fromOrdList [] = E
+fromOrdList xs = let n = length xs
+                     m = div n 2
+                     x = xs !! m
+                     zs = take m xs
+                     ys = drop (m+1) xs
+                     (t1,t2) = (fromOrdList zs fromOrdList ys)
+                     in N t1 x t2 
+
+
+data Heap a = E | N a (Heap a) (Heap a)
+
+xs = [2,5,4,3]
+
+[N 2 E E, N 5 E E, N 4 E E, N 3 E E]
+    t1      t2        t3       t4
+map (\x-> N x E E) xs
+
+[merge t1 t2, merge t3 t4]
+     t1'           t2'
+[merge t1' t2']
+      t1''
+
+fromList [] = e
+fromList zs = let hs = map (\x-> N x E E) zs
+                  pares [] = []
+                  pares [x]  
+                  pares (x:y:xs) = merge x y : pares xs
+                  g [] = E
+                  g [h] = h
+                  g ys = g(pares ys)
+                  in g hs
+
+
+
+
+
+
+
