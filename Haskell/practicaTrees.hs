989ea8d500c45c26a487f395eaf59b409131cc96
francobramucci <bramuccifranco1@gmail.com>
2023-05-22 19:27:05 -0300

    selec

--- Haskell/practicaTrees.hs
+++ Haskell/practicaTrees.hs
@@ -5,7 +5,7 @@ import Data.List
 --1 
 data Nat = Cero | Succ Nat deriving Show
 
---a) Ninguno, es un constructor
+--a) Succ es constructor
 
 --b)
 int2Nat 0 = Cero
@@ -19,4 +19,29 @@ suma (Succ x) y = suma x (Succ y)
 nat2Int Cero = 0
 nat2Int (Succ x) = nat2Int x + 1
 
---e)
+--2)
+
+data Arb = E | H Int | N Arb Arb deriving Show
+
+data Cmd = L | R deriving(Show,Eq)
+
+--a) N es constructor
+--b)
+
+-- selec xs E = E
+-- selec [] arb = arb
+-- selec (x:xs) (N (H a) der) = if(x == R) then selec xs der else (H a)
+-- selec (x:xs) (N izq (H b)) = if(x == L) then selec xs izq else (H b)
+-- selec (x:xs) (N izq der) = if(x == L) then selec xs izq else selec xs der 
+
+
+selec [] arb = arb  
+selec (L:xs) (N izq der) = selec xs izq
+selec (R:xs) (N izq der) = selec xs der 
+
+enum (N E (H b)) = 
+enum (N (H a) E) = 
+enum (N izq der) = L : enum izq 
+
+
+
